d0dc2be385390d32448ef3e59ef9043f5723d19c
Anders <anders.jipwouo@gmail.com>
2021-12-21 21:14:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./*.{vue,js,ts}"
+  ],
+  media:false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
